80726e82288878f4e6455a10f1b24531a4e3ec79
Ilja <ilushpajev@gmail.com>
2021-11-13 16:03:11 +0200

    TMS file lesson 4

---
+++ src/com/TMS/Lesson4/Main.java
@@ -0,0 +1,14 @@
+package com.TMS.Lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1 mySeason = Task1.Summer;
+        System.out.println(Task1.Summer.month);
+        System.out.println(Task1.Winter.month);
+        System.out.println(Task1.valueOf("Spring"));
+
+
+    }
+
+}
+
