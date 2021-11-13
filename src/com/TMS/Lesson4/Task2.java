b4a649129627859fe041c795d83bcfea1b0f6132
Ilja <ilushpajev@gmail.com>
2021-11-13 16:03:58 +0200

    TMS file lesson 4

---
+++ src/com/TMS/Lesson4/Task2.java
@@ -0,0 +1,29 @@
+package com.TMS.Lesson4;
+
+import java.util.Arrays;
+
+public class Task2 {
+    int [] arr = {14,5,445,678,5,32,3,223,5};
+
+    private void sort(int [] arr){
+
+        }
+
+
+    private  void max(int[] arr){
+        int max;
+        max= arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max){
+                max= arr[i];
+            }
+
+        }
+
+    }
+
+    private void search(){
+
+    }
+
+}
